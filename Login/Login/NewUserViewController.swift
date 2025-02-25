7e3041aacb860f3e9fd33d7d4fbd342b3688cc70
MatiCenturion <maticentul50@gmail.com>
2025-02-25 17:16:04 -0300

    Se agrego boton de retroceso "Back"

--- Login/Login/NewUserViewController.swift
+++ Login/Login/NewUserViewController.swift
@@ -13,9 +13,10 @@ class NewUserViewController: UIViewController {
     @IBOutlet weak var newPasswordTextField: UITextField!
     @IBOutlet weak var newUserTextField: UITextField!
     
+    @IBOutlet weak var tituloLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tituloLabel.text = "Ingrese Nuevo Usuario"
     }
     
 
