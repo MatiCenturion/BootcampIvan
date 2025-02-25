39239a3c35db6a40e7b18f11a00b57837931a052
MatiCenturion <maticentul50@gmail.com>
2025-02-25 17:49:07 -0300

    Implementacion del codigo para agregar new user

--- Login/Login/LoginViewController.swift
+++ Login/Login/LoginViewController.swift
@@ -13,62 +13,9 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var userTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    /*
-    var users: [String: String] = [
-        "usuario1": "password1",
-        "usuario2": "password2",
-        "usuario3": "password3"
-    ]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    // Acción cuando se presiona el botón login
-    @IBAction func loginButtonTapped(_ sender: UIButton) {
-        validateLogin()
-    }
-    
-    func validateLogin() {
-        guard let username = userTextField.text, !username.isEmpty else {
-            showAlert(message: "Por favor ingrese un nombre de usuario.")
-            return
-        }
-        
-        guard let password = passwordTextField.text, !password.isEmpty else {
-            showAlert(message: "Por favor ingrese una contraseña.")
-            return
-        }
-        
-        if let storedPassword = users[username], storedPassword == password {
-            // Si el usuario y la contraseña son correctos, navega a la pantalla principal
-            goToHome()
-        } else {
-            // Si la validación falla, mostramos un mensaje de error
-            showAlert(message: "Usuario o contraseña incorrectos.")
-        }
-    }
-    
-    func showAlert(message: String) {
-        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default))
-        self.present(alert, animated: true, completion: nil)
-    }
-    
-    func goToHome() {
-        // Solo navega si la validación fue exitosa
-        if let homeVC = storyboard?.instantiateViewController(withIdentifier: "homeID") as? HomeViewController {
-            navigationController?.pushViewController(homeVC, animated: true)
-        }
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Este método es opcional para preparar cualquier segue adicional.
-    }
-}*/
     
     // Lista de usuarios y contraseñas válidas
-      let validUsers: [String: String] = [
+      var validUsers: [String: String] = [
           "usuario1": "password1",
           "usuario2": "password2",
           "usuario3": "password3"
@@ -115,7 +62,22 @@ class LoginViewController: UIViewController {
           alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
           present(alert, animated: true, completion: nil)
       }
-  }
+    
+    // MARK: - NewUserDelegate
+        func didAddNewUser(username: String, password: String) {
+            // Agregar el nuevo usuario al diccionario de usuarios válidos
+            validUsers[username] = password
+            print("Nuevo usuario agregado: \(username)")
+        }
+        
+        // Presentar NewUserViewController
+        @IBAction func agregarNuevoUsuario(_ sender: Any) {
+            if let newUserVC = storyboard?.instantiateViewController(withIdentifier: "NewUserViewControllerID") as? NewUserViewController {
+                newUserVC.delegate = self  // Establecer el delegado
+                present(newUserVC, animated: true, completion: nil)
+            }
+        }
+    }
 
 
 
--- Login/Login/NewUserViewController.swift
+++ Login/Login/NewUserViewController.swift
@@ -7,18 +7,40 @@
 
 import UIKit
 
-class NewUserViewController: UIViewController {
-    @IBOutlet weak var aceptarNewUserTextField: UIButton!
-    @IBOutlet weak var cancelarNewUserTextField: UIButton!
-    @IBOutlet weak var newPasswordTextField: UITextField!
-    @IBOutlet weak var newUserTextField: UITextField!
-    
-    @IBOutlet weak var tituloLabel: UILabel!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        tituloLabel.text = "Ingrese Nuevo Usuario"
+    class NewUserViewController: UIViewController {
+        
+        @IBOutlet weak var aceptarNewUserTextField: UIButton!
+        @IBOutlet weak var cancelarNewUserTextField: UIButton!
+        @IBOutlet weak var newPasswordTextField: UITextField!
+        @IBOutlet weak var newUserTextField: UITextField!
+        @IBOutlet weak var tituloLabel: UILabel!
+        
+        weak var delegate: NewUserDelegate? // Agregar delegado
+        
+        protocol NewUserDelegate: AnyObject {
+            func didAddNewUser(username: String, password: String)
+        }
+
+
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            tituloLabel.text = "Ingrese Nuevo Usuario"
+        }
+
+        @IBAction func aceptarNewUser(_ sender: UIButton) {
+            guard let newUser = newUserTextField.text, !newUser.isEmpty,
+                  let newPassword = newPasswordTextField.text, !newPassword.isEmpty else {
+                return
+            }
+            
+            // Notificar al delegado
+            delegate?.didAddNewUser(username: newUser, password: newPassword)
+            self.dismiss(animated: true, completion: nil) // Cerrar la vista después de agregar el usuario
+        }
+        
+        @IBAction func cancelarNewUser(_ sender: UIButton) {
+            self.dismiss(animated: true, completion: nil)
+        }
     }
-    
 
-
-}
