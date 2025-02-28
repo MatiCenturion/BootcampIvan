7dd08ef42fcf8319922a20b5a4aa96c9a114ad24
MatiCenturion <maticentul50@gmail.com>
2025-02-28 15:38:58 -0300

    Se implemento el cambio de password

--- Login/Login/EmailViewController.swift
+++ Login/Login/EmailViewController.swift
@@ -5,26 +5,7 @@
 //  Created by bootcamp on 2025-02-25.
 //
 
-
 /*
-import UIKit
-
-class EmailViewController: UIViewController {
-    
-    @IBOutlet weak var labelEmail: UILabel!
-    @IBOutlet weak var enviarButton: UIButton!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        labelEmail.text = "Le enviaremos un correo de confirmacion"
-    }
-    
-    @objc func backButtonTapped() {
-        self.navigationController?.popViewController(animated: true)
-    }
-
-}
- */
 import UIKit
 
 class EmailViewController: UIViewController {
@@ -61,3 +42,54 @@ class EmailViewController: UIViewController {
         self.navigationController?.popViewController(animated: true)
     }
 }
+*/
+
+
+import UIKit
+
+class EmailViewController: UIViewController {
+    
+    @IBOutlet weak var labelEmail: UILabel!
+    @IBOutlet weak var enviarButton: UIButton!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        labelEmail.text = "Le enviaremos un correo de confirmación"
+    }
+    
+    @IBAction func enviarButtonTapped(_ sender: UIButton) {
+        guard let inputText = emailTextField.text, !inputText.isEmpty,
+              let userData = UserDefaults.standard.dictionary(forKey: "userData") as? [String: String],
+              let savedEmail = userData["email"], let savedUsername = userData["username"] else {
+            mostrarAlerta(titulo: "Error", mensaje: "El email o usuario ingresado no es válido.")
+            return
+        }
+        
+        // Verificar si ingresó el email o el username
+        if inputText == savedEmail {
+            // Guarda el username en UserDefaults si ingresó su email
+            UserDefaults.standard.set(savedUsername, forKey: "selectedUser")
+        } else if inputText == savedUsername {
+            // Guarda el username si ingresó directamente su usuario
+            UserDefaults.standard.set(savedUsername, forKey: "selectedUser")
+        }
+        
+        // Navegar a la pantalla de actualización de contraseña
+       /* if let updatePasswordVC = storyboard?.instantiateViewController(withIdentifier: "UpdatePasswordViewController") as? UpdatePasswordViewController {
+            navigationController?.pushViewController(updatePasswordVC, animated: true)
+        }*/
+    }
+    
+    func mostrarAlerta(titulo: String, mensaje: String) {
+        let alert = UIAlertController(title: titulo, message: mensaje, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @objc func backButtonTapped() {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
+
--- Login/Login/UpdatePasswordViewController.swift
+++ Login/Login/UpdatePasswordViewController.swift
@@ -19,7 +19,7 @@ class UpdatePasswordViewController: UIViewController {
     }
 }
 */
-
+/*
 import UIKit
 
 class UpdatePasswordViewController: UIViewController {
@@ -76,4 +76,70 @@ class UpdatePasswordViewController: UIViewController {
         present(alert, animated: true, completion: nil)
     }
 }
+*/
+import UIKit
 
+class UpdatePasswordViewController: UIViewController {
+
+    @IBOutlet weak var aceptarPasswordButton: UIButton!
+    @IBOutlet weak var cancelPasswordButton: UIButton!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var newPasswordTextField: UITextField!
+    
+    var selectedUser: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Recuperar el usuario guardado en UserDefaults
+        if let username = UserDefaults.standard.string(forKey: "selectedUser") {
+            selectedUser = username
+            print("Usuario seleccionado: \(username)") // Debugging
+        } else {
+            mostrarAlerta(titulo: "Error", mensaje: "No se encontró un usuario válido.")
+        }
+    }
+    
+    @IBAction func aceptarPasswordTapped(_ sender: UIButton) {
+        guard let newPassword = newPasswordTextField.text, !newPassword.isEmpty,
+              let confirmPassword = confirmPasswordTextField.text, !confirmPassword.isEmpty else {
+            mostrarAlerta(titulo: "Error", mensaje: "Los campos de contraseña no pueden estar vacíos.")
+            return
+        }
+        
+        // Validar que ambas contraseñas sean iguales
+        guard newPassword == confirmPassword else {
+            mostrarAlerta(titulo: "Error", mensaje: "Las contraseñas no coinciden.")
+            return
+        }
+        
+        // Obtener los datos guardados en UserDefaults
+        if var userData = UserDefaults.standard.dictionary(forKey: "userData") as? [String: String],
+           let username = selectedUser {
+            
+            // Verificar si el usuario existe en los datos guardados
+            if userData["username"] == username {
+                // Actualizar la contraseña
+                userData["password"] = newPassword
+                
+                // Guardar nuevamente los datos actualizados
+                UserDefaults.standard.set(userData, forKey: "userData")
+                UserDefaults.standard.synchronize()
+                
+                mostrarAlerta(titulo: "Éxito", mensaje: "Contraseña actualizada correctamente.") { _ in
+                    self.navigationController?.popViewController(animated: true)
+                }
+            } else {
+                mostrarAlerta(titulo: "Error", mensaje: "No se encontró el usuario en los datos guardados.")
+            }
+        } else {
+            mostrarAlerta(titulo: "Error", mensaje: "No se encontraron datos de usuario.")
+        }
+    }
+    
+    func mostrarAlerta(titulo: String, mensaje: String, accion: ((UIAlertAction) -> Void)? = nil) {
+        let alert = UIAlertController(title: titulo, message: mensaje, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: accion))
+        present(alert, animated: true, completion: nil)
+    }
+}
